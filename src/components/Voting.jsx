e9864b345200c3852a3c53499140bd2b04b31a58
Siddharth Ramesh <siddharth.ramesh93@gmail.com>
2017-02-14 22:41:02 +0530

    Added a callback for the buttons (#3)

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -4,15 +4,16 @@ class Voting extends Component {
   getPair() {
     return this.props.pair || [];
   }
-  
+
   render() {
     return (
       <div className='voting'>
         {this.getPair().map(entry =>
-          <button key={entry}>
+          <button key={entry}
+                  onClick={() => this.props.vote(entry)}>
             <h1>{entry}</h1>
           </button>
-        )}                
+        )}
       </div>
     );
   }
