a201ac40f4f5612bdf9a248ed3ece2d554b62ffb
Siddharth Ramesh <siddharth.ramesh93@gmail.com>
2017-02-27 22:25:11 +0530

    WIP vote tally

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 
-const Results = () => {
+const Results = (props) => {
+  const getPair = () => {
+    return props.pair || [];
+  }
+
+  const getVotes = (entry) => {
+    if (props.tally && props.tally.has(entry)) {
+      return props.tally.get(entry);
+    }
+    return 0;
+  }
   return (
-    <div>
-      Hello from Results!
+    <div className='results'>
+      {getPair().map(entry =>
+        <div key={entry} className='entry'>
+          <h1>{entry}</h1>
+          <div className='voteCount'>
+            {getVotes(entry)}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
